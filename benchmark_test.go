ead7c0f7d78288af3b0c6b677403f07be4893f23
Steven Zhu <steven.zhu@zionsoft.net>
2015-01-16 14:51:37 +0200

    Added simple benchmark.

---
+++ benchmark_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2015 ZionSoft. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+package lrucache
+
+import (
+	"strconv"
+	"testing"
+)
+
+type bytes []byte
+
+func (b bytes) Size() uint64 {
+	return uint64(cap(b))
+}
+
+func BenchmarkGet(b *testing.B) {
+	c := New(64 * 1024 * 1024)
+	value := make(bytes, 1024)
+
+	for i := 0; i < 1024; i++ {
+		c.Set(strconv.Itoa(i), value)
+	}
+
+	for i := 0; i < b.N; i++ {
+		v := c.Get("512")
+		if v == nil {
+			panic("error")
+		}
+		_ = v
+	}
+}
